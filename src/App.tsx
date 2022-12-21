ec8bd3bb8a2df2d54c0b63baf1e9743f771cb16c
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2022-12-21 16:06:12 +0300

    fix(common):add hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider, HashRouter } from "react-router-dom";
 import { router } from "./router";
 
 export const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <RouterProvider router={router} />
+    </HashRouter>
+  );
 };
