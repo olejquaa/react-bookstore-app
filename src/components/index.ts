fa9b22de6f79555454ef63ae001822526545f3fa
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2022-12-21 13:33:10 +0300

    refactor(styles): HomePage

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { Footer } from "./Footer/Footer";
 import { Logo } from "./Logo/Logo";
 import { Header } from "./Header/Header";
+import { Home } from "./Home/Home";
 
-export { Logo, Footer, Header };
+export { Logo, Footer, Header, Home };
