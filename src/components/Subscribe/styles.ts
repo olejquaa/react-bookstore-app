51f491b8b7c7e7e588e5b222ddcf167a83ec3475
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2023-01-25 22:21:36 +0300

    fix(styles): minor fix

--- src/components/Subscribe/styles.ts
+++ src/components/Subscribe/styles.ts
@@ -1,14 +1,3 @@
-// <StyledSubscribe>
-//   <Title> Subscribe to Newsletter </Title>
-//   <Subtitle>
-//     Be the first to know about new IT books, upcoming releases, exclusive offers and more.
-//   </Subtitle>
-//   <InputContainer>
-//     <Input placeholder="Your Email" />
-//     <Button type="submit">Subscribe</Button>
-//   </InputContainer>
-// </StyledSubscribe>;
-
 import styled from "styled-components";
 import { Color, space, typography } from "ui";
 
