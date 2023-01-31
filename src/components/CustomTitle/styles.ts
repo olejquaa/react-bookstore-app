5abf7a111546fb8b5391f280a8bd7afc406940c1
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2023-01-31 10:27:51 +0300

    fix(styles): custom title

--- src/components/CustomTitle/styles.ts
+++ src/components/CustomTitle/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { mediaQuery, space, typography } from "ui";
 
 const StyledTitle = styled.div`
+  align-self: flex-start;
   ${typography.h1}
   margin-bottom: ${space.desktop.xxlg};
 
